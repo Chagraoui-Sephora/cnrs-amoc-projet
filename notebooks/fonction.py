8250fe3094046bf0354fec8e057cc66a80bbf9dd
CHAGRAOUI <seph@MacBook-Air-de-CHAGRAOUI.local>
2026-07-03 10:08:20 +0400

    MAJ3

--- notebooks/fonction.py
+++ notebooks/fonction.py
@@ -8,9 +8,8 @@ import xarray as xr
 
 
 def extraction_proxies():
-    """
-    Charge le fichier NetCDF et extrait les proxies de surface 
-    """
+    #Charge le fichier NetCDF et extrait les proxies de surface 
+
 
     chemin="../data/glorys_sst_sss_ssh_2015_2024.nc"
     ds=xr.open_dataset(chemin)
@@ -21,37 +20,28 @@ def extraction_proxies():
     return sst, sss, ssh
 
 def moyenne_saisonniere(var, season):
-    """
-    Calcule la moyenne saisonnière, la variable saison vaut "DJF" ou "JJA" 
-    """
+    #Calcule la moyenne saisonnière, la variable saison vaut "DJF" ou "JJA" 
     return var.where(var.time.dt.season == season, drop=True).mean("time")
 
 def climatologie_mensuelle(variable):
-    """
-    Calcule une moyenne pour chaque mois de l'année sur la période 2015-2025
-    """
+    #Calcule une moyenne pour chaque mois de l'année sur la période 2015-2025
     return variable.groupby("time.month").mean("time")
 
 def amplitude_saisonniere(variable):
-    """
-    Calcule l'amplitude saisonnière maximum mensuel climatologique - minimum mensuel climatologique
-    """
+    #Calcule l'amplitude saisonnière : maximum mensuel climatologique - minimum mensuel climatologique
     climatologie = climatologie_mensuelle(variable)
     return climatologie.max("month") - climatologie.min("month")
 
 
 def anomalies_mensuelles(variable):
-    """
-    Calcule les anomalies mensuelles après retrait du cycle saisonnier moyen
-    """
+    #Calcule les anomalies mensuelles après retrait du cycle saisonnier moyen
     climatologie = climatologie_mensuelle(variable)
     return variable.groupby("time.month") - climatologie
 
 
 def gradient(variable):
-    """
-    Calcule la norme du gradient en projetant les dérivées partielles sur la sphère terrestre
-    """
+    #Calcule la norme du gradient en projetant les dérivées partielles sur la sphère terrestre
+    
     R = 6371000  # rayon de la Terre en mètres
     lat_rad = np.deg2rad(variable.latitude)
 
@@ -63,22 +53,20 @@ def gradient(variable):
     return gradient
 
 def variabilite_residuelle(variable):
-    """
-    Calcule la variabilité résiduelle : écart-type des anomalies mensuelles
-    """
+    #Calcule la variabilité résiduelle : écart-type des anomalies mensuelles
     anomalies = anomalies_mensuelles(variable)
     return anomalies.std("time")
 
-#Representation graphique
+#---------------Representation graphique
 
 def echelle_commune(cartes):
-    """Calcule une échelle de couleur commune à plusieurs cartes."""
+    #Calcule une échelle de couleur commune à plusieurs cartes.
     vmin = min(float(carte.min()) for carte in cartes)
     vmax = max(float(carte.max()) for carte in cartes)
     return vmin, vmax
 
 def carte(carte1, titre1, titre1_cbar,carte2=None, titre2=None, titre2_cbar=None,carte3=None, titre3=None, titre3_cbar=None,cmap=None, figsize=(14, 5), same_scale=False):
-    """ Trace 1, 2 ou 3 cartes côte à côte """
+    # Trace 1, 2 ou 3 cartes côte à côte 
     cartes = [c for c in [carte1, carte2, carte3] if c is not None]
     titres = [t for c, t in zip([carte1, carte2, carte3],[titre1, titre2, titre3]) if c is not None]
     titres_cbar = [tc for c, tc in zip([carte1, carte2, carte3],[titre1_cbar, titre2_cbar, titre3_cbar]) if c is not None]
@@ -104,14 +92,14 @@ def carte(carte1, titre1, titre1_cbar,carte2=None, titre2=None, titre2_cbar=None
 #-------------------------------------------------------------
 
 def selection_gulf_stream(variable):
-    """Sélectionne le domaine Gulf Stream."""
+    #Sélectionne le domaine Gulf Stream.
     return variable.sel(latitude=slice(30, 50),longitude=slice(-80, -40))
 
 def degradation_resolution(variable, facteur=3):
-    """Dégradation spatiale en remplacant par la moyenne locale : pour GLORYS 1/12° un facteur=3 donne une pseudo-résolution 1/4°"""
+    #Dégradation spatiale en remplacant par la moyenne locale : pour GLORYS 1/12° un facteur=3 donne une pseudo-résolution 1/4°
     return variable.coarsen(latitude=facteur,longitude=facteur,boundary="trim").mean()
 
 def gradient_p95(variable):
-    """Calcule le quantile 95 du gradient spatial pour avoir les valeurs les plus fortes du gradient"""
+    #Calcule le quantile 95 du gradient spatial pour avoir les valeurs les plus fortes du gradient
     grad_t = gradient(variable)
     return grad_t.quantile(0.95, dim="time")
